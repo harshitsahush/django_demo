1e369ba53f93b41f7bcb2fc9f55e4dab8ef4ec54
Harshit Sahu <harshitsahu.sh@gmail.com>
2024-09-24 19:42:42 +0530

    Added simple template

--- project1/project1/views.py
+++ project1/project1/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Youre in homepage")
+    #return HttpResponse("Youre in homepage")
+    return render(request, "index.html")
 
 def about(request):
     return HttpResponse("Youre in about section")
