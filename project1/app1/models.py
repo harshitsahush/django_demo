5952dd058f069ae5eb0824883068a0b44ab3a24e
Harshit Sahu <harshitsahu.sh@gmail.com>
2024-09-26 18:31:50 +0530

    Created a model for app1 and migrated

--- project1/app1/models.py
+++ project1/app1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=50)
+    date_added = models.DateTimeField(default = timezone.now)
