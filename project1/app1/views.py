02adb446b02604218e0a10a4ed8b46d4e69a2586
Harshit Sahu <harshitsahu.sh@gmail.com>
2024-09-26 19:43:16 +0530

    Fetched data from DB and show in page

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import post
 
 # Create your views here.
 def index(request):
-    return render(request, "app1_index.html")
+    #fetch all posts from db and show in app1_index
+    posts = post.objects.all
+    return render(request, "app1_index.html", {"posts" : posts})
