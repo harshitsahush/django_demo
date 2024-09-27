87e641d2413d0942e9e2003031a4495ef1d7e017
Harshit Sahu <harshitsahu.sh@gmail.com>
2024-09-27 19:17:23 +0530

    Implemented all 3 types of relationships

--- project1/app1/admin.py
+++ project1/app1/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import post
+from .models import post, ChaiVariety, ChaiReview, Store, ChaiCert
 
 # Register your models here.                
-admin.site.register(post)               #shows post model in admin panel
+admin.site.register(ChaiVariety)               #shows post model in admin panel
+admin.site.register(ChaiReview) 
+admin.site.register(Store) 
+admin.site.register(ChaiCert) 
--- project1/app1/models.py
+++ project1/app1/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User             #django maintains a default DB of users
 
 # Create your models here.
 class post(models.Model):
@@ -10,3 +11,47 @@ class post(models.Model):
 
     def __str__(self):
         return self.title           #will show the title name in admin panel instead of "post object"
+    
+
+#create a baisc example model
+class ChaiVariety(models.Model):
+    name = models.CharField(max_length=100)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+
+# One to Many
+# One type of chai can have many reviews
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete = models.CASCADE)           #
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    rating = models.IntegerField()
+    comments = models.TextField()
+    date_added = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} review for {self.chai.name}"
+    
+
+# Many to Many
+# A store can have many chai varieties and vice versa
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varieties = models.ManyToManyField(ChaiVariety)
+
+    def __str__(self):
+        return self.name
+
+
+# One to One
+# A certificate is unique to a chai
+class ChaiCert(models.Model):
+    chai = models.OneToOneField(ChaiVariety, on_delete = models.CASCADE)
+    cert_num = models.CharField(max_length=100)
+    issued_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"Cert for {self.chai.name}"
