7444af6c756d13db8d19d64d558f455810c7b879
Harshit Sahu <harshitsahu.sh@gmail.com>
2024-09-30 13:18:47 +0530

    Getting data from a form and using it to get and display data from DB

--- project1/app1/urls.py
+++ project1/app1/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('post/<int:post_id>/', views.post_detail, name = "post_detail")
+    path('post/<int:post_id>/', views.post_detail, name = "post_detail"),
+    path('chai_stores/', views.chai_store_view),
 ]
--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import post
+from .models import post, ChaiVariety, Store
 from django.shortcuts import get_object_or_404
+from .forms import ChaiVarietyForm
 
 # Create your views here.
 def index(request):
@@ -12,3 +13,16 @@ def post_detail(request, post_id):
     #fetch post with post_id
     temp = get_object_or_404(post, pk = post_id)
     return render(request, "post_page.html", {"post" : temp})
+
+def chai_store_view(request):
+    stores = None
+    if(request.method == 'POST'):
+        #form submit request
+        form = ChaiVarietyForm(request.POST)
+        if(form.is_valid()):
+            chai_variety = form.cleaned_data["chai_variety"]              #chai_variety is field name in forms.py
+            stores = Store.objects.filter(chai_varieties = chai_variety)
+    else:
+        form = ChaiVarietyForm()                #if simple get request, render form
+    
+    return render(request, 'chai_stores.html', {'form' : form, "stores" : stores})
